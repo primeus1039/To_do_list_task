9092581093f5d864b34db9f50b5cdfc410e397da
Navin1039 <primeus1039@github.com>
2026-04-12 16:27:33 +0800

    updated up to task 17

--- readPerson.js
+++ readPerson.js
@@ -5,6 +5,8 @@ import { text } from 'node:stream/consumers';
 console.clear()
 
 const personData = personJsonData
+const getImageFolderPath = "assets"
+const getImages = await readdir(getImageFolderPath)
 
 async function accessPersonData(personArray) {
     for (let indexDetails = 0; indexDetails < personArray.length; indexDetails++) {
@@ -17,3 +19,18 @@ async function accessPersonData(personArray) {
     }
 }
 accessPersonData(personData)
+
+const allImagesPath = [];
+
+for (const index of getImages) {
+    console.log(index);
+    console.log(getImageFolderPath)
+    const fullImagePath = getImageFolderPath + '/' + index;
+    console.log(fullImagePath)
+    // && is the AND operator, || is the OR operator
+    if (!fullImagePath.endsWith('jpg') && !fullImagePath.endsWith('png')) continue;
+    allImagesPath.push(fullImagePath);
+
+}
+console.log(allImagesPath)
+console.log()
